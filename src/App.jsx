65756b1b08506aa2adc47a496564bdd17740c24c
Mahamudur10 <mahamudur649@gmail.com>
2026-03-31 00:23:40 +0600

    fetch data from json file

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import GetStartedSection from './components/GetStartedSection'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 import PricingSection from './components/PricingSection'
+import ProductSection from './components/ProductSection'
 import StatsSection from './components/StatsSection'
 
 function App() {
-  
+
 
   return (
     <>
       <Navbar></Navbar>
       <HeroSection></HeroSection>
       <StatsSection></StatsSection>
+      <ProductSection></ProductSection>
       <GetStartedSection></GetStartedSection>
       <PricingSection></PricingSection>
       <CtaSection></CtaSection>
