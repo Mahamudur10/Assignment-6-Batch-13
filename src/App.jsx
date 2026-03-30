78a67140511c0c050be1d2ff91d4255466e6278c
Mahamudur10 <mahamudur649@gmail.com>
2026-03-30 19:36:02 +0600

    start assignment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-2xl'>hellow</h1>
+      <button className="btn btn-xl">Xlarge</button>
+    </>
+  )
+}
+
+export default App
